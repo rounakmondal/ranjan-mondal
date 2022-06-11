2402e9b9d16402379f01490604932611bf5d61ab
rounakmondal <rounakmondal198@gmail.com>
2022-06-11 13:13:42 -0700

    changes

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,7 +3,7 @@
   <footer id="footer">
     <div class="container">
       <h3>Ranjan Mondal</h3>
-      <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
+ 
       <div class="social-links">
         <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
         <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
@@ -15,10 +15,7 @@
         &copy; Copyright <strong><span>MyResume</span></strong>. All Rights Reserved
       </div>
       <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: [license-url] -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/ -->
+   
         Designed by <a href="#">Ron</a>
       </div>
     </div>
